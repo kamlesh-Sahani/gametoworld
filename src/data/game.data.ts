8e1e72ccded3cfeaaa9cc45de9fe8d41c9d272c1
kamlesh-Sahani <alex20150001969@gmail.com>
2025-05-02 23:34:29 +0530

    chnages

--- src/data/game.data.ts
+++ src/data/game.data.ts
@@ -22,4 +22,100 @@ export const gameDetail:GameDetailType[] = [
         bigSize:false
       
     },
+    {
+        _id:1,
+        name:"Game To World",
+        imgUrl:"https://images.unsplash.com/photo-1591154669695-5f2a8d20c089?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        redirectUrl:"/game-to-world",
+        bigSize:true
+      
+    },
+    {
+        _id:2,
+        name:"Game To World",
+        imgUrl:"https://media.istockphoto.com/id/2151954310/photo/shield-security-network-technology-wire-frame-concept.jpg?s=1024x1024&w=is&k=20&c=7mRKh765UWhFqCi9vkJtvBGL3fqaOJdr4qjvjyqi_ZU=",
+        redirectUrl:"/game-to-world",
+        bigSize:false
+      
+    },
+    {
+        _id:1,
+        name:"Game To World",
+        imgUrl:"https://images.unsplash.com/photo-1591154669695-5f2a8d20c089?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        redirectUrl:"/game-to-world",
+        bigSize:true
+      
+    },
+    {
+        _id:2,
+        name:"Game To World",
+        imgUrl:"https://media.istockphoto.com/id/2151954310/photo/shield-security-network-technology-wire-frame-concept.jpg?s=1024x1024&w=is&k=20&c=7mRKh765UWhFqCi9vkJtvBGL3fqaOJdr4qjvjyqi_ZU=",
+        redirectUrl:"/game-to-world",
+        bigSize:false
+      
+    },
+    {
+        _id:1,
+        name:"Game To World",
+        imgUrl:"https://images.unsplash.com/photo-1591154669695-5f2a8d20c089?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        redirectUrl:"/game-to-world",
+        bigSize:true
+      
+    },
+    {
+        _id:2,
+        name:"Game To World",
+        imgUrl:"https://media.istockphoto.com/id/2151954310/photo/shield-security-network-technology-wire-frame-concept.jpg?s=1024x1024&w=is&k=20&c=7mRKh765UWhFqCi9vkJtvBGL3fqaOJdr4qjvjyqi_ZU=",
+        redirectUrl:"/game-to-world",
+        bigSize:false
+      
+    },
+    {
+        _id:1,
+        name:"Game To World",
+        imgUrl:"https://images.unsplash.com/photo-1591154669695-5f2a8d20c089?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        redirectUrl:"/game-to-world",
+        bigSize:true
+      
+    },
+    {
+        _id:2,
+        name:"Game To World",
+        imgUrl:"https://media.istockphoto.com/id/2151954310/photo/shield-security-network-technology-wire-frame-concept.jpg?s=1024x1024&w=is&k=20&c=7mRKh765UWhFqCi9vkJtvBGL3fqaOJdr4qjvjyqi_ZU=",
+        redirectUrl:"/game-to-world",
+        bigSize:false
+      
+    },
+    {
+        _id:1,
+        name:"Game To World",
+        imgUrl:"https://images.unsplash.com/photo-1591154669695-5f2a8d20c089?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        redirectUrl:"/game-to-world",
+        bigSize:true
+      
+    },
+    {
+        _id:2,
+        name:"Game To World",
+        imgUrl:"https://media.istockphoto.com/id/2151954310/photo/shield-security-network-technology-wire-frame-concept.jpg?s=1024x1024&w=is&k=20&c=7mRKh765UWhFqCi9vkJtvBGL3fqaOJdr4qjvjyqi_ZU=",
+        redirectUrl:"/game-to-world",
+        bigSize:false
+      
+    },
+    {
+        _id:1,
+        name:"Game To World",
+        imgUrl:"https://images.unsplash.com/photo-1591154669695-5f2a8d20c089?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        redirectUrl:"/game-to-world",
+        bigSize:true
+      
+    },
+    {
+        _id:2,
+        name:"Game To World",
+        imgUrl:"https://media.istockphoto.com/id/2151954310/photo/shield-security-network-technology-wire-frame-concept.jpg?s=1024x1024&w=is&k=20&c=7mRKh765UWhFqCi9vkJtvBGL3fqaOJdr4qjvjyqi_ZU=",
+        redirectUrl:"/game-to-world",
+        bigSize:false
+      
+    },
 ]
